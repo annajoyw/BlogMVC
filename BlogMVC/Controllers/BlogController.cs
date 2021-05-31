7e9b7e7fd0c19e132517d6bafea52b12370bf9ab
annajoyw <arealjoyw@gmail.com>
2021-05-30 20:12:14 -0400

    Add project files.

---
+++ BlogMVC/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogMVC.Controllers
+{
+    [Route("blog")]
+    public class BlogController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{year:int}/{month:int}/{key}")]
+        public IActionResult Post(int year, int month, string key)
+        {
+            //return new ContentResult { Content = id };
+        
+        }
+    }
+}
